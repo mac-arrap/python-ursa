2edb5e3f875351b53988a7a8ce8fd3844d1ca70e
Cam Parra <caeparra@gmail.com>
2019-03-21 16:24:10 -0600

    [CAM-001] added and edited files from old wrapper code
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
 
-try:
-    long_description = open("README.rst").read()
-except IOError:
-    long_description = ""
+from setuptools import setup
 
 setup(
-    name="ursa",
-    version="0.1.0",
-    description="python wrapper for ursa universal crypto library",
-    license="apache2",
-    author="Cam Parra",
-    packages=find_packages(),
-    install_requires=["pytest"],
-    long_description=long_description,
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-    ]
+    name='ursa',
+    version='0.1.0',
+    url='https://github.com/cam-parra/python-ursa',
+    description='python wrapper for ursa universal crypto library',
+    license='Apache-2.0',
+    author='Cam Parra',
+    author_email='camilo.parra@evernym.com',
+    packages=['ursa'],
+    install_requires=['pytest'],
+    tests_require=['pytest']
 )
